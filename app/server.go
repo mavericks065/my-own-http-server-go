6cb16c611754e440fc1938c014581cfd681d05cc
Nicolas Guignard <nicolas065@gmail.com>
2023-10-12 18:34:58 +0200

    feat: make stage 3  pass

--- app/server.go
+++ app/server.go
@@ -34,17 +34,27 @@ func main() {
 	}
 
 	req := string(buf[:requestBytes])
-	fmt.Println("Server Request decoded: ", req)
+	fmt.Println("SERVER REQUEST DECODED: ", req)
 
 	requestPath := strings.Split(req, " ")[1]
-	fmt.Println("Request path: ", requestPath)
+	uriParts := strings.Split(requestPath, "/")
+	header := "HTTP/1.1 200 OK\r\n"
+	contentType := "Content-Type: text/plain\r\n"
+	var responseContent []string
 
-	var serverResponse []byte
-	if requestPath != "/" {
-		serverResponse = []byte("HTTP/1.1 404 Not Found response\r\n\r\n")
+	if requestPath == "/" {
+		responseContent = []string{header, "\r\n"}
+	} else if uriParts[1] == "echo" {
+		body := strings.TrimPrefix(requestPath, "/echo/")
+		contentLength := fmt.Sprintf("Content-Length: %d\r\n\r\n", len(body))
+		responseContent = []string{header, contentType, contentLength, body, "\r\n"}
+
 	} else {
-		serverResponse = []byte("HTTP/1.1 200 OK\r\n\r\n")
+		header = "HTTP/1.1 404 Not Found response\r\n\r\n"
+		responseContent = []string{header}
 	}
+	fmt.Println("RESPONSE CONTENT: ", strings.Join(responseContent, ""))
+	serverResponse := []byte(strings.Join(responseContent, ""))
 	_, err = connection.Write(serverResponse)
 	if err != nil {
 		fmt.Println("Error writing response: ", err.Error())
