33be188d20c0f3a4ec645d72264fa78dca8c3c42
Nicolas Guignard <nicolas065@gmail.com>
2023-10-23 18:33:50 +0200

    feat: make stage 5 pass

--- app/server.go
+++ app/server.go
@@ -36,7 +36,24 @@ func main() {
 	req := string(buf[:requestBytes])
 	fmt.Println("SERVER REQUEST DECODED: ", req)
 
-	requestPath := strings.Split(req, " ")[1]
+	responseContent := handleRequest(req)
+
+	fmt.Println("RESPONSE CONTENT: ", strings.Join(responseContent, ""))
+	serverResponse := []byte(strings.Join(responseContent, ""))
+	_, err = connection.Write(serverResponse)
+	if err != nil {
+		fmt.Println("Error writing response: ", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("data sent")
+	connection.Close()
+}
+
+func handleRequest(req string) []string {
+	fmt.Println("IDEEEEEEEEEEEEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAS")
+	requestRows := strings.Split(req, "\r\n")
+
+	requestPath := strings.Split(requestRows[0], " ")[1]
 	uriParts := strings.Split(requestPath, "/")
 	header := "HTTP/1.1 200 OK\r\n"
 	contentType := "Content-Type: text/plain\r\n"
@@ -48,18 +65,19 @@ func main() {
 		body := strings.TrimPrefix(requestPath, "/echo/")
 		contentLength := fmt.Sprintf("Content-Length: %d\r\n\r\n", len(body))
 		responseContent = []string{header, contentType, contentLength, body, "\r\n"}
-
+	} else if uriParts[1] == "user-agent" {
+		var body string
+		for _, row := range requestRows {
+			if strings.Contains(row, "User-Agent") {
+				body = strings.Split(row, " ")[1]
+				continue
+			}
+		}
+		contentLength := fmt.Sprintf("Content-Length: %d\r\n\r\n", len(body))
+		responseContent = []string{header, contentType, contentLength, body, "\r\n"}
 	} else {
 		header = "HTTP/1.1 404 Not Found response\r\n\r\n"
 		responseContent = []string{header}
 	}
-	fmt.Println("RESPONSE CONTENT: ", strings.Join(responseContent, ""))
-	serverResponse := []byte(strings.Join(responseContent, ""))
-	_, err = connection.Write(serverResponse)
-	if err != nil {
-		fmt.Println("Error writing response: ", err.Error())
-		os.Exit(1)
-	}
-	fmt.Println("data sent")
-	connection.Close()
+	return responseContent
 }
